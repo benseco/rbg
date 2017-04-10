9153e6cc8dea3ed873dee0035d503655bb27a516
Ben Seco <ben.seco@live.com>
2017-04-10 17:12:56 -0500

    dialogue with words now

--- client/public/lib/static/Dialogue.ts
+++ client/public/lib/static/Dialogue.ts
@@ -2,19 +2,24 @@ class Dialogue
 {
 
     times: number = 0;
-    sprite: Phaser.Sprite;
+    //sprite: Phaser.Sprite;
+    text: Phaser.Text;
+    style: object;
 
     // Temporary isAdvancing flag
-    isAdvancing: boolean = false;
+    isAdvancing: boolean = true;
 
     constructor(key?: string) 
     {
 
         G.game.paused = true;
 
-        this.sprite = G.getSprite('testplayer', 0, 0);
-        this.sprite.fixedToCamera = true;
-        
+        //this.sprite = G.getSprite('testplayer', 0, 0);
+        //this.sprite.fixedToCamera = true;
+
+        this.style = { font: 'bold 60pt Arial', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: 450 };
+        this.text = G.game.add.text(0, 0, "(0/5) phaser with a sprinkle of pixi dust", this.style);
+        this.text.fixedToCamera = true;
 
         this.times = 0;
         
@@ -34,6 +39,8 @@ class Dialogue
                 {
                     this.end();
                 }
+                
+                this.text.text = "(" + this.times + "/5) " + "phaser with a sprinkle of pixi dust";
 
                 // Temporary isAdvancing flag
                 this.isAdvancing = true;
@@ -45,7 +52,7 @@ class Dialogue
 
     private end()
     {
-        this.sprite.kill();
+        this.text.kill();
         G.game.paused = false;
         G.pauseUpdate.remove(this.PauseUpdate, this);
     }
